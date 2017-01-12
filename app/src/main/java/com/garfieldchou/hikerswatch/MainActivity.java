cb314fc835c71eeff90d863d7ba6868a226e9021
garfield_chou <garfield_chou@asus.com>
2017-01-12 17:42:09 +0800

    Ask for permission to ACCESS_FINE_LOCATION and requestLocationUpdates()

--- app/src/main/java/com/garfieldchou/hikerswatch/MainActivity.java
+++ app/src/main/java/com/garfieldchou/hikerswatch/MainActivity.java
@@ -1,9 +1,14 @@
 package com.garfieldchou.hikerswatch;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -41,6 +46,23 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onProviderDisabled(String provider) {
+
+            }
+        }
+
+        if (Build.VERSION.SDK_INT < 23) {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            
+        } else {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                
+            } else {
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
             }
         }
