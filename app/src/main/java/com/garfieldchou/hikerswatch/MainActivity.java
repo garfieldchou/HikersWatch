23e31ff4fb86a8a5caaf8c03aa9424162e8f267c
garfield_chou <garfield_chou@asus.com>
2017-01-12 17:21:44 +0800

    Setup locationManager and locationListener

--- app/src/main/java/com/garfieldchou/hikerswatch/MainActivity.java
+++ app/src/main/java/com/garfieldchou/hikerswatch/MainActivity.java
@@ -1,13 +1,49 @@
 package com.garfieldchou.hikerswatch;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                Log.i("LocationInfo", location.toString());
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        }
+
     }
 }
